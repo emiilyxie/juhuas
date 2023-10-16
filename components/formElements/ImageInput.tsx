fc389d49c0c622360b207a35f7139a2e945b9a60
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-10-15 21:42:36 -0700

    validate image input size

--- components/formElements/ImageInput.tsx
+++ components/formElements/ImageInput.tsx
@@ -4,11 +4,23 @@ export function ImageInput(props : {onSelect : (img : File) => void}) {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
   const displayImg = (e : React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
+    if (e.target.files && e.target.files.length > 0 && checkFileSize(e.target.files[0])) {
       setSelectedImage(e.target.files[0]);
       props.onSelect(e.target.files[0]);
+    } else {
+      e.target.value = ""
+      setSelectedImage(null)
     }
-  };
+  }
+
+  const checkFileSize = (file : File) => {
+      const fileSize = file.size / 1024 / 1024; // in MB
+      if (fileSize > 4) {
+        alert("File size exceeds 4 MB");
+        return false
+      }
+    return true
+  }
 
   return (
     <div>
